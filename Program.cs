88d5080bc27492a1dbba2448aacce8387795287c
Alena <zemlemerova.as@gmail.com>
2022-10-02 10:42:20 +0500

    created initial array and counter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Test()
+{
+    string[] words = {"hello", "2", "world",":-)"};
+    PrintArray(words);
+    int count = 0;
+    Console.WriteLine("Слова не более 3х символов:");
+    for (int i = 0; i < words.Length; i++)
+    {  
+        if (words[i].Length <= 3)
+        {                        
+            Console.Write(words[i] + " ");
+            count++;                        
+        }  
+    }
+    Console.WriteLine();
+    Console.WriteLine("Количество слов не более 3х символов: " + count);
+    
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Test();
